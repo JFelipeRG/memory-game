bbe46219352e2b091340ed2e87c859cab98bd973
Juan Felipe Ramirez <juanfel2024@gmail.com>
2023-04-18 12:04:28 +0200

    Valor por defecto en record

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@ export default {
   },
   beforeMount() {
     this.randomizeBoard()
-    this.record = parseInt(localStorage.record)
+    this.record = parseInt(localStorage.record) || 0
   },
   watch: {
     randomly: function () {
